c6e1fdad367c8b168df6661a270dffcf3a6d9780
Biprajit Ghoshal <126811815+bg-on-github@users.noreply.github.com>
2024-11-22 01:12:27 +0530

    Delete painter directory

--- painter/urls.py
+++
@@ -1,8 +0,0 @@
-# painter/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('video_feed/', views.video_feed, name='video_feed'),
-]
